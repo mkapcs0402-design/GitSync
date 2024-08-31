6f319cf23f229a2bba9cf983f26f84ac81e8a2e0
ViscousPotential <viscouspotential@gmail.com>
2024-08-31 19:57:38 +0100

    fix: applications not refreshing after turning observer off and on

--- app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
+++ app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
@@ -319,6 +319,7 @@ class MainActivity : AppCompatActivity() {
                     requestAccessibilityPermission()
                 } else {
                     settingsManager.setApplicationObserverEnabled(true)
+                    refreshSelectedApplications()
                 }
             } else {
                 settingsManager.setApplicationObserverEnabled(false)
