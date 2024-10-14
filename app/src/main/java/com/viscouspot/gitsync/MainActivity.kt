88287e1af6e41102a97b7627f67033d7c67304c0
ViscousPotential <viscouspotential@gmail.com>
2024-10-14 22:34:42 +0100

    feat: focus app search bar when dialog is opened

--- app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
+++ app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
@@ -442,6 +442,7 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.adapter = adapter
         dialog.show()
+        searchView.requestFocus()
     }
 
     private fun refreshAll() {
