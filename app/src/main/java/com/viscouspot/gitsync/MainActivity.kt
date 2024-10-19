88e290ec9778dbcc664b7566b35b93f923b287d0
ViscousPotential <viscouspotential@gmail.com>
2024-10-19 20:06:06 +0100

    feat: disable setings button when repo not selected

--- app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
+++ app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
@@ -743,6 +743,7 @@ class MainActivity : AppCompatActivity() {
 
                 applicationObserverSwitch.isChecked = false
                 applicationObserverSwitch.isEnabled = false
+                settingsButton.isEnabled = false
 
                 if (gitDirPath.text.isEmpty()) {
                     gitRepoName.rightDrawable(null)
@@ -764,6 +765,7 @@ class MainActivity : AppCompatActivity() {
 
             gitRepoName.setText(repoName)
             gitRepoName.isEnabled = true
+            settingsButton.isEnabled = true
             gitRepoName.rightDrawable(R.drawable.circle_check)
             gitRepoName.compoundDrawableTintList = getColorStateList(R.color.auth_green)
             gitRepoName.compoundDrawablePadding =
