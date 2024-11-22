ddb446bcdf2fc602ea80d38ebf5c89546e80272d
ViscousPot <viscouspotential@gmail.com>
2024-11-22 00:14:44 +0000

    chore: cleanup

--- app/src/main/java/com/viscouspot/gitsync/ui/fragment/CloneRepoFragment.kt
+++ app/src/main/java/com/viscouspot/gitsync/ui/fragment/CloneRepoFragment.kt
@@ -21,7 +21,6 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
-import com.viscouspot.gitsync.BuildConfig
 import com.viscouspot.gitsync.R
 import com.viscouspot.gitsync.ui.adapter.RepoListAdapter
 import com.viscouspot.gitsync.util.GitManager
