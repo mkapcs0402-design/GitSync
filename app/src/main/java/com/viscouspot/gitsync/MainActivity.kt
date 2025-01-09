7c859423ff8d106d9a493e619d4c108f91bac9ba
ViscousPot <viscouspotential@gmail.com>
2025-01-09 19:01:39 +0000

    feat: jump between conflicts using arrow keys

--- app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
+++ app/src/main/java/com/viscouspot/gitsync/MainActivity.kt
@@ -696,7 +696,6 @@ class MainActivity : AppCompatActivity() {
         val gitDirUri = settingsManager.getGitDirUri()
         gitDirUri?.let {
             val recentCommitsReferences = recentCommits.map { commit -> commit.reference }
-            log(settingsManager.getGitDirUri())
             val newRecentCommits = gitManager.getRecentCommits(Helper.getPathFromUri(this, it))
                 .filter { commit -> !recentCommitsReferences.contains(commit.reference) }
             if (newRecentCommits.isNotEmpty()) {
